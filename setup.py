933eb474ac58319f1e9f0496053f4f86997bb562
Adam Hill <ahill@fool.com>
2020-02-12 19:49:16 -0500

    Make sure readme is used on pypi.

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from setuptools import setup, find_packages
+from os import path
 
 install_requires = ["wagtail"]
 
 tests_require = ["pytest-django", "wagtail-factories", "pytest"]
 
+with open(
+    path.join(path.abspath(path.dirname(__file__)), "README.md"), encoding="utf-8"
+) as f:
+    long_description = f.read()
+
 setup(
     version="1.0.0",
     name="wagtail-automatic-redirects",
     description="Helpers for Wagtail Redirects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Parbhat Puri",
     author_email="me@parbhatpuri.com",
     url="https://github.com/themotleyfool/wagtail-automatic-redirects",
@@ -27,4 +35,5 @@ setup(
         "Framework :: Wagtail",
     ],
     setup_requires=["setuptools_scm", "pytest-runner"],
+    python_requires=">=3.6",
 )
