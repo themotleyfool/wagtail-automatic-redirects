12ef35c97caf12431a757bd80d3fb8ff588f3025
Tibor Leupold <tibor.leupold@torchbox.com>
2020-12-01 10:44:32 -0800

    Test moving page created redirect
    Adding a test to aid development of the feature to create automatic
    redirects for cases when a page is moved. This feature has previously
    not been available, due to missing signals.
    
    The `pre_page_move` and `post_page_move` signals are available since
    Wagtail 2.10. Using these to implement the automatic redirect creation
    is the goal of this branch.

--- tests/test_pages.py
+++ tests/test_pages.py
@@ -95,3 +95,55 @@ def test_index_page_slug_change_create_redirects_child_pages(client, site):
 
     response = client.get('/index-page/test-page-2/')
     assert response.status_code == 301
+
+
+@pytest.mark.django_db
+def test_move_page_from_one_index_to_another_creates_redirect(client, site):
+    test_index_page_1 = factories.AutomaticRedirectsTestIndexPageFactory(
+        parent=site.root_page,
+        title='Automatic redirects test index page 1',
+        slug='index-page-1',
+        subtitle='Test subtitle',
+        body='<p>Test body</p>',
+    )
+
+    response = client.get(test_index_page_1.get_url())
+    assert response.status_code == 200
+
+    test_index_page_2 = factories.AutomaticRedirectsTestIndexPageFactory(
+        parent=site.root_page,
+        title='Automatic redirects test index page 2',
+        slug='index-page-2',
+        subtitle='Test subtitle',
+        body='<p>Test body</p>',
+    )
+
+    response = client.get(test_index_page_2.get_url())
+    assert response.status_code == 200
+    assert 'index-page-2' in test_index_page_2.get_url()
+
+    test_page = factories.AutomaticRedirectsTestPageFactory(
+        parent=test_index_page_1,
+        title='Test Page',
+        slug='test-page'
+    )
+
+    test_page_url_pre_move = test_page.get_url()
+    response = client.get(test_page_url_pre_move)
+    assert response.status_code == 200
+    assert test_index_page_1.get_url() in test_page_url_pre_move
+
+    test_page.move(test_index_page_2, 'last-child')
+    test_page.refresh_from_db()
+
+    test_page_url_post_move = test_page.get_url()
+    assert test_index_page_1.get_descendants().count() == 0
+    assert test_index_page_2.get_descendants().count() == 1
+    assert test_index_page_1.get_url() not in test_page_url_post_move
+    assert test_index_page_2.get_url() in test_page_url_post_move
+
+    response = client.get(test_page_url_post_move)
+    assert response.status_code == 200
+    response = client.get(test_page_url_pre_move)
+    assert response.status_code == 301
+
