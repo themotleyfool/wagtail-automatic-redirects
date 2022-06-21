34664b1fbd2f6558fa223c9a447f63bada792738
Brian Xu <brian.xu@torchbox.com>
2022-06-21 17:30:25 +0800

    use path instead of url

--- tests/urls.py
+++ tests/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import include, url
+from django.urls import include, path
 from wagtail import VERSION as WAGTAIL_VERSION
 
 from wagtail.admin import urls as wagtailadmin_urls
@@ -10,6 +10,6 @@ else:
 
 
 urlpatterns = [
-    url(r'^admin/', include(wagtailadmin_urls)),
-    url(r'', include(wagtail_urls)),
+    path("admin/", include(wagtailadmin_urls)),
+    path("", include(wagtail_urls)),
 ]
